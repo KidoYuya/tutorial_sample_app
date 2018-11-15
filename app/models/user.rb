9edbe6d883497640c764a6f2baa72241886e603e
KidoYuya <prideofisho92@gmail.com>
2018-11-15 01:05:16 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :microposts, dependent: :destroy
     attr_accessor :remember_token
     before_save {email.downcase!}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -33,5 +34,9 @@ class User < ApplicationRecord
     
     def forget
         update_attribute(:remember_digest, nil)
+    end
+    
+    def feed 
+        Micropost.where("user_id = ?", id)
     end
 end
